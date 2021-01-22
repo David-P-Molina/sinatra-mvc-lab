65aa11455080e1515dca792e5a7270fad65baa67
David Moina <davemolina777@gmail.com>
2021-01-22 12:09:44 -0600

    commit tree 1acd5811aaf3a6647f4876a20b077e4538b617b4

---
+++ models/piglatinizer.rb
@@ -0,0 +1,30 @@
+require 'pry'
+class PigLatinizer
+    #boiler plate methods
+    # @@all = []
+    # attr_accessor :name, :id
+    # def initialize(name:, id:)
+    #     @name = name
+    #     @id = nil
+    #     save
+    # end
+    # def save
+    #     @@all << self
+    # end
+    #requireed information
+    def split_string(string)
+        binding.pry
+        #produce array of strings
+    end
+
+    # def iterates_through_array(array)
+    #     pig_latin_array = array.collect do |word|
+    #         if #word begins with a consonant move letter to end and ad -ay at end
+    #         else #add way at end
+    #         end
+    #     end
+    # end
+    def piglatinizes(array) 
+    #takes array and converts to a string
+    end
+end
